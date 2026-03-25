3c6e727ef3706cf5262cfce6cfe5747dce76544c
Matheus Augusto Cremonez Guimarães <matheus_acg@hotmail.com>
2026-03-25 15:40:05 -0300

    Adjusting vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Use relative paths so the built game can be hosted easily on any subpath (like GitHub Pages)
-  base: './',
+  base: '/morty-parasite-protocol/',
 });
